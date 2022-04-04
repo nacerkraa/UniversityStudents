ddc4e3f624af087438cb7ce5733af2e60864a448
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-04-04 23:11:20 +0000

    add a new om_university module

---
+++ custom_addons/om_university/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Hospital Management",
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'summary': "Hospital management system",
+    'description': """Hospital management system""",
+    'author': 'Nacer KRAA',
+    'depends': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/patient_view.xml',
+    ],
+    'demo': [],
+    'sequence': -100,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+    'website': "http://www.yourcompany.com",
+
+}
+
