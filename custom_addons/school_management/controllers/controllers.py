2cc2dfca0ed87a4c5ef584250ef5222da8545b5b
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-04-12 15:29:35 +0000

    create hello world page in odoo 15$

---
+++ custom_addons/school_management/controllers/controllers.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+
+
+class TestWebsite(http.Controller):
+    @http.route('/test_website/test_website', auth='public', website=True)
+    def index(self, **kwargs):
+        return "Hello, world"
+
+    # @http.route('/test_website/test_website/objects', auth='public')
+    # def list(self, **kw):
+    #     return http.request.render('test_website.listing', {
+    #         'root': '/test_website/test_website',
+    #         'objects': http.request.env['test_website.test_website'].search([]),
+    #     })
+
+#     @http.route('/test_website/test_website/objects/<model("test_website.test_website"):obj>', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('test_website.object', {
+#             'object': obj
+#         })
