a8a134d9082a84c6d2ed4dd4537a55cf664b2aaf
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-04-12 19:03:49 +0000

    Defining the data model

--- custom_addons/academy/controllers/controllers.py
+++ custom_addons/academy/controllers/controllers.py
@@ -2,12 +2,14 @@
 
 from odoo import http
 
+
 class Academy(http.Controller):
 
     @http.route('/academy/academy/', auth='public')
     def index(self, **kw):
+        Teachers = http.request.env['academy.teachers']
         return http.request.render('academy.index', {
-            'teachers': ["Diana Padilla", "Jody Caroll", "Lester Vaughn"],
+            'teachers': Teachers.search([])
         })
 
 #     @http.route('/academy/academy/objects', auth='public')
