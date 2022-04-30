b55e54f81e7ddc8557a2741f9a1841abe5e11e3e
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-04-30 16:06:01 +0000

    Compute Field for the years of study

--- custom_addons/om_university/models/student.py
+++ custom_addons/om_university/models/student.py
@@ -1,5 +1,5 @@
+from  datetime import date
 from odoo import api, fields, models
-
 class UniversityStudent(models.Model):
     _name = 'university.student'
     _description = 'University Student'
@@ -9,11 +9,22 @@ class UniversityStudent(models.Model):
 
     ref = fields.Char(string="Ref")
     name = fields.Char(string="Name")
-    age = fields.Integer(string="Age")
+    age = fields.Integer(string="Age", compute='_cumpute_years')
     gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender")
     email = fields.Char(string="Email")
     phone = fields.Char(string="¨Phone")
     faculty_id = fields.Many2one('university.faculty', string="Faculty")
     rate = fields.Float(string="Rate")
-    establish_date = fields.Datetime(string="Establish Date", default=fields.Datetime.now)
+    establish_date = fields.Date(string="Establish Date")
     active = fields.Boolean(string="Active", default=True)
+
+    @api.depends('establish_date')
+    def _cumpute_years(self):
+        for rec in self:
+            today = date.today()
+            if rec.establish_date:
+                rec.age = today.year - rec.establish_date.year
+            else:
+                rec.age = 0
+
+
