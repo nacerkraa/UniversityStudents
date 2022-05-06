fa9e3149b0828345ece352116fb0572f5f643b54
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-05-06 12:23:34 +0000

    update files

---
+++ custom_addons/om_university/controllers/controllers.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+from odoo.http import request
+
+class Academy(http.Controller):
+
+    @http.route('/test_website/test_website', auth='public', website=True)
+    def hello(self, **kw):
+        return "Hello, world"
+
+    @http.route('/academy/academy/', auth='public', website=True)
+    def index(self, **kw):
+        Students = http.request.env['university.student']
+        return http.request.render('om_university.index', {
+            'students': Students.search([])
+        })
+
+class School(http.Controller):
+
+    @http.route('/student_webform', type="http", auth="public", website=True)
+    def student_webform(self, **kw):
+        Students = http.request.env['university.student']
+        return http.request.render('om_university.create_student', {'students': Students.search([])})
+
+    @http.route('/create/webstudent', type="http", auth="public", website=True)
+    def create_webpatient(self, **kw):
+        request.env['university.student'].sudo().create(kw)
+        return request.render("om_university.student_thanks", {})
+
