bace46e31a17066ce02dcad46cc846d0356344db
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-05-06 14:49:07 +0000

    update list

--- custom_addons/om_university/models/univ.py
+++ custom_addons/om_university/models/univ.py
@@ -2,7 +2,7 @@ from odoo import api, fields, models
 
 class univs(models.Model):
     _name = 'university.univ'
-    _description = 'University '
+    _description = 'University'
 
     ref = fields.Char(string="Ref")
     name = fields.Char(string="Name")
