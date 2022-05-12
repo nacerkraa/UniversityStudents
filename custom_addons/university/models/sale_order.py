59301807da9c36db282714596beb398b79fbb7b6
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-05-12 22:39:35 +0000

    inherit sale module

--- custom_addons/university/models/sale_order.py
+++ custom_addons/university/models/sale_order.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class university(models.Model):
-#     _name = 'university.university'
-#     _description = 'university.university'
+class SaleOrder(models.Model):
+    _name = 'university.university'
+    _inherit = 'sale.order'
+    _description = 'university.university'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+
+    @api.depends('value')
+    def _value_pc(self):
+        for record in self:
+            record.value2 = float(record.value) / 100
