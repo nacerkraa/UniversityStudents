bd213a4b627e7ce8e9cc897b41bec7b6cecd0e2d
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-05-12 22:45:21 +0000

    fix the error of the instalation

--- custom_addons/university/models/sale_order.py
+++ custom_addons/university/models/sale_order.py
@@ -4,16 +4,14 @@ from odoo import models, fields, api
 
 
 class SaleOrder(models.Model):
-    _name = 'university.university'
     _inherit = 'sale.order'
-    _description = 'university.university'
 
     name = fields.Char()
-    value = fields.Integer()
-    value2 = fields.Float(compute="_value_pc", store=True)
-    description = fields.Text()
+    # value = fields.Integer()
+    # value2 = fields.Float(compute="_value_pc", store=True)
+    # description = fields.Text()
 
-    @api.depends('value')
-    def _value_pc(self):
-        for record in self:
-            record.value2 = float(record.value) / 100
+    # @api.depends('value')
+    # def _value_pc(self):
+    #     for record in self:
+    #         record.value2 = float(record.value) / 100
