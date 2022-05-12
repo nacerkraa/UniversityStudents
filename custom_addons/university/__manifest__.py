fe08315b6949931947a8dcc6c8c030ee0f5a6995
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-05-12 22:32:21 +0000

    add the depandacesis

--- custom_addons/university/__manifest__.py
+++ custom_addons/university/__manifest__.py
@@ -13,22 +13,18 @@
     'author': "My Company",
     'website': "http://www.yourcompany.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
 
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['sale'],
 
-    # always loaded
+
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
-    # only loaded in demonstration mode
+
     'demo': [
         'demo/demo.xml',
     ],
